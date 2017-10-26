14da2981bae36f3d64ccea4e96c303cc37b64498
lyxuefeng <1316939854@qq.com>
2017-10-26 19:44:54 +0800

    add notes

--- binbin/task4/task4.js
+++ binbin/task4/task4.js
@@ -7,16 +7,16 @@ var section = document.getElementById('section');
 
 //左侧入
 leftIn.addEventListener('click',function(){
-  if(text.value == ''){
+  if(text.value == ''){//input里为空格时
     alert('请输入一个数字');
-  }else if(!isNaN(text.value)){
+  }else if(!isNaN(text.value)){//输入的为数字时
     var newLi = document.createElement('li');
     // console.log(newLi);
     var textNode = document.createTextNode(text.value);
-    newLi.appendChild(textNode);
-    section.insertBefore(newLi,section.firstChild);
+    newLi.appendChild(textNode);//把input文本添加到li里
+    section.insertBefore(newLi,section.firstChild);//把li添加为ul的第一个子元素
   }else{
-    alert('类型错误，请输入数字！');
+    alert('类型错误，请输入数字！');//输入的不是数字
   }
 });
 
@@ -29,7 +29,7 @@ rightIn.addEventListener('click',function(){
     // console.log(newLi);
     var textNode = document.createTextNode(text.value);
     newLi.appendChild(textNode);
-    section.insertBefore(newLi,null);
+    section.insertBefore(newLi,null);//把li添加为ul的最后一个子元素
   }else{
     alert('类型错误，请输入数字！');
   }
@@ -37,15 +37,15 @@ rightIn.addEventListener('click',function(){
 
 //左侧出
 leftOut.addEventListener('click',function(){
-  var firstRemove = section.children[0];
+  var firstRemove = section.children[0];//获取ul的第一个子元素
   // console.log(firstRemove);
-  section.removeChild(firstRemove);
+  section.removeChild(firstRemove);//删除第一个子元素
   alert(firstRemove.innerHTML);
 });
 
 //右侧出
 rightOut.addEventListener('click',function(){
-  var lastRemove = section.children[section.children.length-1];
+  var lastRemove = section.children[section.children.length-1];//获取ul的最后一个子元素
   // console.log(lastRemove);
   section.removeChild(lastRemove);
   alert(lastRemove.innerHTML);
@@ -56,7 +56,7 @@ section.addEventListener('click',function(){
   // for(var i = 0;i<section.children.length;i++){
   //   section.removeChild(event.target);
   // }
-  if(event.target.nodeName.toLowerCase() == 'li'){
+  if(event.target.nodeName.toLowerCase() == 'li'){//判断点击元素是li
     section.removeChild(event.target);
   }
 });
@@ -66,5 +66,6 @@ section.addEventListener('click',function(){
 
 
 
-// 为什么不能用value全局变量直接代替text.value？
-//在事件代理删除中，为什么for循环的方法虽然可以实现删除，可是出错？
+//1. 为什么不能用value全局变量直接代替text.value？
+//2.在事件代理删除中，为什么for循环的方法虽然可以实现删除，可是出错？
+/*回答2：猜想:用for遍历了ul的所有li,但是里面执行的跟li没有关系，只是限定删除点击元素，而且因为for循环执行多次（然而第一次已经删除，所以出错）。*/
